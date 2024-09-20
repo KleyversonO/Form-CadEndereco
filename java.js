01be09dd312354c66eb80a79f076369ff29aa256
KleyversonO <kleyversonoliveira@gmail.com>
2024-09-20 07:33:24 -0300

    Edição do JavaScript, para que haja verificação do CEP. E início da implementação do código para que haja o funcionamento do código que fará com que o CEP pegue e verifique o CEP digitado e diga a localidade e informações do local

--- java.js
+++ java.js
@@ -10,7 +10,17 @@ const limparformulario = () =>{
     document.getElementById('estado').value = '';
 }
 
-//Verifica se o CEP é válido
+//Verifica se o CEP é válido. Esse código tem a função de verificar se o que foi digitado são apenas números.
 const Enumero = (numero) => /^[0-9]+$/.test(numero);
-// Verifica o tamanho do CEP
+// Verifica o tamanho do CEP. Esse código tem a função de verificar se o tamanho do código possui 8 digitos.
 const cepValido = (cep) => cep.length == 8 && Enumero(cep);
+
+// Função para preencher campos relacionados ao CEP
+const preencherFormulario = (endereco) =>{
+    document.getElementById('rua').value = endereco.logradouro;    //Coloca o valor de logradouro da API dentro do campo rua do formulario
+    document.getElementById('bairro').value = endereco.bairro;    //Coloca o valor de bairro da API dentro do campo bairro do formulario
+    document.getElementById('cidade').value = endereco.localidade;    //Coloca o valor de localidade da API dentro do campo cidade do formulario
+    document.getElementById('estado').value = endereco.uf;  //Coloca o valor de uf da API dentro do campo estado do formulario
+
+
+}
